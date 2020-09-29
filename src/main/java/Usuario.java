5ceb96a8747b681bba89c196b9aeff9ebcab97b1
Jhon Jairo Castaño <jjcastan@unal.edu.co>
2020-09-29 17:03:20 -0500

    Archivos Iniciales

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+
+    public int cedula;
+    public String nombre;
+    public String apellidos;
+    public String correo;
+    public String contraseña;
+    public String rol;
+
+    public Usuario(int cedula, String nombre, String apellidos, String correo, String contraseña, String rol) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+
+}
+
+
