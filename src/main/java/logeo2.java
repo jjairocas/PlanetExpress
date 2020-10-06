0abc2442b48e7b99585769810d7347a6bffdf5be
Carlos <cnhurtadom@unal.edu.co>
2020-10-06 15:16:09 -0500

    archivos login

---
+++ src/main/java/logeo2.java
@@ -0,0 +1,19 @@
+
+public class logeo2 {
+
+    public static void ingresar(Usuario user){
+
+    }
+
+    public static  void registrarse(Usuario user){
+
+    }
+
+    public static void validarCedula(Usuario user){
+
+    }
+
+    public static void validarPassword(Usuario user){
+
+    }
+}
